a6a4a6ace78f02b78f27069d6af7f5a067f8d2b0
fyuan <208793439@qq.com>
2023-05-19 02:28:57 +0800

    init

---
+++ libTest/libProject/Hardware/led/bsp_led1.c
@@ -0,0 +1,14 @@
+
+
+#include "bsp_led1.h"
+
+void led_gpio_config(void)
+{
+  /* 使能时钟 */
+	rcu_periph_clock_enable(RCU_LED1);
+	/* 配置GPIO的模式 */
+	gpio_mode_set(PORT_LED1,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,PIN_LED1);
+	/* 配置GPIO的输出 */
+	gpio_output_options_set(PORT_LED1,GPIO_OTYPE_PP,GPIO_OSPEED_50MHZ,PIN_LED1);
+}
+
