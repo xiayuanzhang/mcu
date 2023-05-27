4e42e6101f7bea9d82a896a6d57f861a31877406
fyuan <208793439@qq.com>
2023-05-28 03:24:21 +0800

    增加注释

--- Systick非阻塞延时/User/systick.c
+++ Systick非阻塞延时/User/systick.c
@@ -49,7 +49,13 @@ uint32_t systick_getms(void)
 }
 
 
-
+/**
+ * @brief 非阻塞延时函数
+ * 
+ * @param timeBuff 时间缓存, 在调用该函数的地方, 使用 static int timeBuff = 0; 进行定义, &timeBuff 传入该函数
+ * @param delayms 要延时的周期,时间
+ * @return int 返回当前周期的剩余时间, 如果返回值为0,表示该周期完成,会进入到下一个周期
+ */
 int systick_delay(int *timeBuff, int delayms)
 {
 	//如果时间有所改变,就进行初始话时间
